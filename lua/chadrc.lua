eeee9649a7479baf2972efc89208a7f5e3c0d174
Marco Alberto Diosdado Nava <alberto.diosdado@valorcreativo.onmicrosoft.com>
2024-06-04 08:28:42 -0600

    Enable relative numbers

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,19 +1,19 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "catppuccin",
+  theme = "catppuccin",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
   ------------------------------- nvchad_ui modules -----------------------------
   statusline = {
-    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    theme = "minimal", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "round",
@@ -73,6 +73,7 @@ M.ui = {
       border = "single",
     },
   },
+
 }
 
 return M
